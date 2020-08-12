135412c642cde700ca98cfda5ec72e8096525efc
stfate <stfate@gmail.com>
2020-08-13 01:44:29 +0900

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -36,7 +36,7 @@ class Header extends React.Component {
     return (
       <React.Fragment>
         <header className={`header ${this.getHeaderSize()}`}>
-          <Link to="/about" className="logoType">
+          <Link to="/" className="logoType">
             <div className="logo">
               <img src={config.gravatarImgMd5=="" ? avatar : config.gravatarImgMd5 } alt={config.siteTitle} />
             </div>
