049143b08b39467707e69597ba942f485422be7b
stfate <stfate@gmail.com>
2022-04-10 23:14:45 +0900

    twitter cardの設定更新

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,6 +71,10 @@ function SEO({ description, lang, meta, title, image }) {
           name: `twitter:description`,
           content: metaDescription,
         },
+        {
+          name: `twitter:image`,
+          content: image
+        },
       ].concat(meta)}
     />
   )
