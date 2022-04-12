b688d0ced58263929b31947213b57baf2bcb0234
stfate <stfate@gmail.com>
2022-04-12 20:26:45 +0900

    Twitter cardsのURL誤りを変更

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -11,8 +11,11 @@ import 'katex/dist/katex.min.css'
 class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
-    const twitterCardImageUrl = "http://tyrfing.site/assets/twitter_cards/" + post.fields.slug + "/twitter_card.png";
+    const twitterCardImageUrl = "http://tyrfing.site/assets/twitter_cards" + post.fields.slug + "twitter_card.png";
     // const twitterCardImageUrl = "http://tyrfing.site/assets/twitter_cards/atlanta-braves-2021-preview/twitter_card.png";
+
+    console.log(post.fields.slug);
+    console.log(twitterCardImageUrl);
 
     return (
       <DefaultLayout>
